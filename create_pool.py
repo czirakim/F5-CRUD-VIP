095ec36b0d03ccd88549e69cb1073528249d081b
mihai cziraki <cziraki.mihai@gmail.com>
2023-02-02 15:36:24 +0200

    first commit

---
+++ create_pool.py
@@ -0,0 +1,34 @@
+import requests
+from rich import print
+import os
+import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+"""
+Create pool
+"""
+
+
+def create_pool():
+
+    API_string = os.environ.get('Authorization_string')
+    url = "https://192.168.88.100/mgmt/tm/ltm/pool"
+    headers = {
+        'Authorization': f'Basic {API_string}',
+        'Content-Type': 'application/json'
+             }
+
+    # Open the file for reading
+    with open('pool.json', 'r') as file:
+        # Read the contents of the file
+        data = file.read()
+
+    # Parse the JSON data
+    items = json.loads(data)
+    for item in items:
+        print(item)
+        payload = json.dumps(item)
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        response.raise_for_status()
---
+++ create_vip.py
@@ -0,0 +1,42 @@
+import requests
+from rich import print
+import os
+import json
+from create_pool import create_pool
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+"""
+Create Virtual Server
+"""
+
+
+def create_vip():
+
+    API_string = os.environ.get('Authorization_string')
+    url = "https://192.168.88.100/mgmt/tm/ltm/virtual"
+    headers = {
+        'Authorization': f'Basic {API_string}',
+        'Content-Type': 'application/json'
+             }
+
+    # Open the file for reading
+    with open('virtual.json', 'r') as file:
+        # Read the contents of the file
+        data = file.read()
+
+    # Parse the JSON data
+    items = json.loads(data)
+    for item in items:
+        print(item)
+        payload = json.dumps(item)
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        response.raise_for_status()
+
+
+if __name__ == "__main__":
+    create_pool()
+    create_vip()
+#    import ipdb
+#    ipdb.set_trace()
