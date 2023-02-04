b83b1817da07fcb606545da8c7c9864d3a3ca731
mihai cziraki <cziraki.mihai@gmail.com>
2023-02-04 09:24:29 +0200

    commit 7

--- graph.py
+++ graph.py
@@ -2,14 +2,14 @@ from pyvis.network import Network
 import json
 
 net = Network()
-obj_list = []
 
 
 # add pools,nodes and vips
-def add_pool_node():
+def add_obj():
     nodes = 0
     pools = 500
     vips = 1000
+    net.repulsion(node_distance=100, spring_length=200)
 
     with open('pool.json', 'r') as file:
         # Read the contents of the file
@@ -19,7 +19,6 @@ def add_pool_node():
     # Parse the JSON data
     items = json.loads(data)
 
-
     with open('virtual.json', 'r') as file:
         # Read the contents of the file
         data = file.read()
@@ -28,35 +27,31 @@ def add_pool_node():
     # Parse the JSON data
     itemsY = json.loads(data)
 
+    # vips
     for it in itemsY:
         vips = vips + 1
         label = it['name']
         net.add_node(vips, label=label, color='#3155a8')
         string = it['pool'].replace('/Common/', '')
 
+        # vip --> pool
         for item in items:
-   #         print(item, pools)
             if (string == item['name']):
                 pools = pools + 1
-                obj_list.append((vips, pools))    
                 label = item['name']
                 net.add_node(pools, label=label, color='#3da831')
                 members = item['members']
+                net.add_edges([(vips, pools, 5)])
 
-
-            # nodes of the pools
+                # pool --> nodes
                 for member in members:
                     nodes = nodes + 1
                     label2 = member['name']
                     net.add_node(nodes, label=label2, color='#9a31a8')
-                    obj_list.append((pools, nodes))
-
-    net.add_edges(obj_list)
-  #  print(obj_list)
+                    net.add_edges([(pools, nodes, 2)])
 
 
 if __name__ == "__main__":
-    add_pool_node()
+    add_obj()
     net.show('nodes.html')
     net.show('edges.html')
-    net.show_buttons(filter_=['physics'])
