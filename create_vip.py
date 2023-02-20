7bbec749ff928716781c2b836e15f0c3b7f3c166
mihai cziraki <cziraki.mihai@gmail.com>
2023-02-20 12:57:31 +0200

    final

--- create_vip.py
+++ create_vip.py
@@ -1,5 +1,6 @@
 """
 Create Virtual Server
+
 """
 
 
--- graph.py
+++ graph.py
@@ -1,15 +1,49 @@
+"""
+Create graph
+
+"""
+
 from pyvis.network import Network
 import os
 import sys
 import json
+import re
 
 net = Network()
+
+# options for graph
+options = {
+  "edges": {
+    "color": {
+      "inherit": True
+    },
+    "font": {
+      "align": "top"
+    },
+    "selfReferenceSize": 0,
+    "selfReference": {
+      "angle": 0.7853981633974483
+    },
+    "smooth": False
+  },
+  "layout": {
+    "hierarchical": {
+      "enabled": True,
+      "levelSeparation": -200,
+      "direction": "DU",
+      "sortMethod": "directed",
+      "shakeTowards": "roots"
+    }
+  }
+  }
+
 
 # Get the current working directory and build the path for teh json file
 cwd = os.getcwd()
 path = f"{cwd}/{sys.argv[1]}"
 
 
+# read a file
 def read_file(name_file):
     with open(f'{name_file}', 'r') as file:
         # Read the contents of the file
@@ -20,20 +54,14 @@ def read_file(name_file):
     return data
 
 
-def remove_speacial_char(special_string):
-    special_characters = ['@', '#', '$', '*', '&', '\\n', '}']
-    normal_string = special_string
-    for i in special_characters:
-        # Replace the special character with an empty string
-        normal_string = normal_string.replace(i, "")
-        # Print the string after the removal of special characters
-    return normal_string
-
-
-def vip2nodes(vips, pools, nodes, string, items, label_string):
+# add nodes and edges to the graph: vip-->pool-->nodes
+def vip2nodes(vips, pools, nodes, pool_name, items, label_string):
     # vip --> pool
     for item in items:
-        if (string == item['name']):
+        if 'else' in pool_name:
+            pool_name = pool_name.replace('else ', '')
+            label_string = label_string + ' else '
+        if (pool_name == item['name']):
             pools = pools + 1
             label1 = item['name']
             label2 = item['loadBalancingMode']
@@ -48,7 +76,22 @@ def vip2nodes(vips, pools, nodes, string, items, label_string):
                 label1 = member['name']
                 net.add_node(nodes, label=label1, color='#9a31a8')
                 net.add_edges([(pools, nodes, 2)])
-    return pools, nodes        
+    return pools, nodes
+
+
+# get the uri in if statements
+def get_uri_pool_IF(item):
+    tmp = re.findall(r"\[HTTP::uri\](.*) ", item)
+    tmp1 = tmp[0].split('"')
+    uri = 'uri:'+tmp1[1]
+    pool = re.findall(r"pool (.*?) ", item)
+    else_pool = re.findall(r"else(.*) pool (.*?) ", item)
+    for i in range(len(pool)):
+        for n in range(len(else_pool)):
+            if pool[i] in else_pool[n]:
+                pool[i] = 'else ' + pool[i]
+                # print(pool[i])
+    return uri, pool
 
 
 # add pools,nodes and vips
@@ -68,33 +111,31 @@ def add_obj():
     for it in itemsY:
         vips = vips + 1
         label1 = it['name']
+        # draw if vip has default pool
         if 'pool' in it:
             string = it['pool'].replace('/Common/', '')
             label2 = it['destination'].replace('/Common/', '')
             label3 = f"{label1} \n {label2}"
             net.add_node(vips, label=label3, color='#3155a8')
-
-        #    vip2nodes(vips, pools, nodes, string, items)
             pools, nodes = vip2nodes(vips, pools, nodes, string, items, 'default')
-                  
+        # draw if vip has irules
         if 'rules' in it:
             irule_file = f"{path}/irules.json"
             itemI = read_file(irule_file)
-            for i in itemI:
-                irule_name = i['name']
-                search_string = "pool"
-                next_string = None
-                output = json.dumps(i['apiAnonymous'])
-                search_index = output.find(search_string)
-                if search_index != -1:
-                    # If the search string is found, try to read the next string
-                    next_index = search_index + len(search_string)
-                    next_string = output[next_index:].split()[0]
-                    pool_name = remove_speacial_char(next_string)
-                    pools, nodes = vip2nodes(vips, pools, nodes, pool_name, items, irule_name)
+            for item in itemI:
+                data1 = item["apiAnonymous"]
+                # irule_name = item["name"]
+                uri, pool = get_uri_pool_IF(data1)
+                print(pool)
+                for name in pool:
+                    # with uri
+                    pools, nodes = vip2nodes(vips, pools, nodes, name, items, uri)
+                    # with irule name
+                    # pools, nodes = vip2nodes(vips, pools, nodes, name, items, irule_name)
 
 
 if __name__ == "__main__":
     add_obj()
     net.show_buttons(filter_=True)
+    net.options.__dict__.update(options)
     net.show(f'{sys.argv[1]}/edges_{sys.argv[1]}.html')
